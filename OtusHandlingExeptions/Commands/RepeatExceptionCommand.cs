36be65ec99e9eefc0a6a0cd954828bb96fdbdaeb
Oleksandr Dobrynin <shuron1988@gmail.com>
2022-03-30 20:26:02 +0300

    added algo and tests

---
+++ OtusHandlingExeptions/Commands/RepeatExceptionCommand.cs
@@ -0,0 +1,17 @@
+﻿namespace OtusHandlingExeptions.Commands
+{
+    public class RepeatExceptionCommand : ICommand
+    {
+        private readonly ExceptionCommand _command;
+
+        public RepeatExceptionCommand(ExceptionCommand command)
+        {
+            _command = command;
+        }
+
+        public void Execute()
+        {
+            _command.Execute();
+        }
+    }
+}
